95f2dc1846821d01b79971a324272ccdb490ffd2
Liam Healy <lhealy0711@gmail.com>
2020-10-03 11:14:19 -0400

    Added map component and dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// import ReactMapGL from 'react-map-gl';
+
+import ReactMapBoxGl, { Layer, Feature } from 'react-mapbox-gl';
+import DrawControl from 'react-mapbox-gl-draw';
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+
+class App extends Component {
+
+  state = {
+    viewport: {
+      width: 600,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState((prevState) => {
+        return {
+          viewport: {
+            ...prevState.viewport,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          }
+        }
+      })
+    })
+  }
+
+  onDrawCreate = ({ features }) => {
+    console.log(features);
+  };
+
+  onDrawUpdate = ({ features }) => {
+    console.log(features);
+  };
+  
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Tripify</h1>
+        <h2>The ReactMapBoxGl Component</h2>
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: this.state.viewport.height,
+            width: this.state.viewport.width
+          }}
+          center={[
+            this.state.viewport.longitude,
+            this.state.viewport.latitude
+          ]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <DrawControl onDrawCreate={this.onDrawCreate} onDrawUpdate={this.onDrawUpdate} />
+        </Map>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+const Map = ReactMapBoxGl({
+  accessToken: process.env.REACT_APP_MAPBOX_TOKEN
+})
